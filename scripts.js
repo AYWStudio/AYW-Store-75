4b9c7e94144b68c3e897a02c0b2588ead678c866
AYWOfficialStudio <aywofficialstudio75@gmail.com>
2025-05-21 10:59:40 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Toggle menu for mobile
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
